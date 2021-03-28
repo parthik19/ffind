999aad42cafe76cf9a3f6f2d9923b08e4c8db038
parthik19 <parthik.j.patel@gmail.com>
2021-03-27 21:55:29 -0400

    implemented fuzzy search

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use colored::*;
 use jwalk::WalkDir;
 use std::path;
 
-pub fn par_search(query: String, dir: path::PathBuf) -> Result<Vec<path::PathBuf>> {
+pub fn par_search(
+    query: String,
+    dir: path::PathBuf,
+    use_fuzzy_search: bool,
+) -> Result<Vec<path::PathBuf>> {
     let mut paths = vec![];
 
     for entry in WalkDir::new(dir) {
@@ -14,8 +18,8 @@ pub fn par_search(query: String, dir: path::PathBuf) -> Result<Vec<path::PathBuf
                     .to_str()
                     .expect("Failed parsing path to string");
 
-                // TODO introduce fuzzy search
-                if file_name == query {
+                // TODO make sure is_close does NOT get evaluated if use_fuzzy_search is false
+                if file_name == query || (use_fuzzy_search && is_close(file_name, &query)) {
                     paths.push(entry.path())
                 }
             }
@@ -30,4 +34,8 @@ pub fn par_search(query: String, dir: path::PathBuf) -> Result<Vec<path::PathBuf
     }
 
     Ok(paths)
+}
+
+fn is_close(a: &str, b: &str) -> bool {
+    strsim::normalized_levenshtein(a, b) >= 0.75
 }
