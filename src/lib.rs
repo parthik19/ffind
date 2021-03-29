72f7fd9c17eef574021fdbf56e4f589669938a7f
parthik19 <parthik.j.patel@gmail.com>
2021-03-29 15:40:15 -0400

    addressing feedback

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,29 @@
-use anyhow::Result;
 use colored::*;
 use jwalk::WalkDir;
 use std::path;
 
-pub fn par_search(
-    query: String,
-    dir: path::PathBuf,
-    fuzzy_threshold: Option<f64>,
-) -> Result<Vec<path::PathBuf>> {
+pub fn par_search(query: String, dir: path::PathBuf, fuzzy_threshold: Option<f64>) -> Vec<String> {
     let mut paths = vec![];
 
     for entry in WalkDir::new(dir) {
         match entry {
             Ok(entry) => {
-                let file_name = entry
-                    .file_name
-                    .to_str()
-                    .expect("Failed parsing path to string");
+                if let Some(file_name) = entry.file_name.to_str() {
+                    let is_match = if let Some(threshold) = fuzzy_threshold {
+                        is_close(file_name, &query, threshold)
+                    } else {
+                        file_name == query
+                    };
 
-                let is_match = if let Some(threshold) = fuzzy_threshold {
-                    is_close(file_name, &query, threshold)
+                    if is_match {
+                        if let Some(file_path) = entry.path().to_str() {
+                            paths.push(file_path.to_string());
+                        } else {
+                            eprintln!("{}", "Failed parsing file path".red());
+                        }
+                    }
                 } else {
-                    file_name == query
-                };
-
-                if is_match {
-                    paths.push(entry.path());
+                    eprintln!("{}", "Failed parsing file name.".red());
                 }
             }
             Err(e) => {
@@ -38,7 +36,7 @@ pub fn par_search(
         }
     }
 
-    Ok(paths)
+    paths
 }
 
 fn is_close(a: &str, b: &str, threshold: f64) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use colored::*;
 use std::env;
 use std::path;
@@ -16,26 +15,24 @@ struct CLInput {
     fuzzy: Option<u8>,
 }
 
-fn main() -> Result<()> {
+fn main() {
     let input = CLInput::from_args();
 
     let start = Instant::now();
     let ffind_result = ffind::par_search(
         input.query,
-        input
-            .starting_dir
-            .unwrap_or_else(|| env::current_dir().expect("Failed determining current directory")),
+        input.starting_dir.unwrap_or_else(|| {
+            env::current_dir()
+                .expect("Failed determining current directory or invalid start directory provided.")
+        }),
         input.fuzzy.map(|x| x as f64 / 100.0),
-    )?;
+    );
     let duration = start.elapsed().as_millis();
 
     if !ffind_result.is_empty() {
-        for path in ffind_result {
-            println!(
-                "{}",
-                path.to_str().expect("Failed parsing path").cyan().bold()
-            );
-        }
+        ffind_result.iter().for_each(|path| {
+            println!("{}", path.cyan().bold());
+        })
     } else {
         println!("{}", "No file matching that query found.".magenta());
     }
@@ -43,6 +40,4 @@ fn main() -> Result<()> {
     println!("");
 
     println!("Took {} msecs.", duration);
-
-    Ok(())
 }
